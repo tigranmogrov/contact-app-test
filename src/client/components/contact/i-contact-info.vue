2bb2b6b9f015236b93fdb034c7f17d1ea4b5f0ae
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-23 19:44:01 +0300

    feat(Components): created contact info

---
+++ src/client/components/contact/i-contact-info.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { IContact, INameContact } from '@/types';
+
+const { telephone, personalization, email, employeeId } = defineProps<IContact>();
+
+const fullName = (name: INameContact) => Object.values(name).join(' ');
+const regexp = /\*(.*?)\d*/g;
+const phone = !telephone.number.match(regexp) ? `+${telephone.number}` : telephone.number;
+</script>
+
+<template>
+  <div>
+    <h1 class="title is-size-5">
+      {{ fullName(personalization.name) }}
+    </h1>
+    <p>
+      <span class="has-text-grey">Телефон:</span>
+      {{ phone }}
+    </p>
+    <p>
+      <span class="has-text-grey">E-mail:</span>
+      {{ email?.address ? `${email.address}` : 'No email' }}
+    </p>
+    <p>
+      <span class="has-text-grey">Є співробітником:</span>
+      {{ employeeId ? 'Так' : 'Ні' }}
+    </p>
+  </div>
+</template>
+
+<style scoped></style>

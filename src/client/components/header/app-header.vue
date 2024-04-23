8ad2354b787c1155503d71c54a30c0a4d37dfc0e
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-23 19:25:47 +0300

    feat(Components): create folder for header and add header

---
+++ src/client/components/header/app-header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouteNameEnum } from '@/enums/router.enum.ts';
+</script>
+
+<template>
+  <header class="app-header">
+    <div class="app-container">
+      <div class="app-header__inner">
+        <RouterLink
+          :to="{ name: RouteNameEnum.Home }"
+          class="app-header__logo has-text-dark is-size-5"
+        >
+          ContactApp
+        </RouterLink>
+
+        <RouterLink :to="{ name: RouteNameEnum.Contacts }" class="button is-small is-dark">
+          До списку контактів
+        </RouterLink>
+      </div>
+    </div>
+  </header>
+</template>

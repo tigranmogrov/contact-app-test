f02aa1148f7eca822009287af8f177a0cedc8cba
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-24 18:29:30 +0300

    refactor(Views): script and template order changed

--- src/client/views/contact.view.vue
+++ src/client/views/contact.view.vue
@@ -1,3 +1,75 @@
+<script setup lang="ts">
+import IContactInfo from '@/components/contact/i-contact-info.vue';
+import IUpdateContact from '@/components/edit-form/i-update-contact.vue';
+import ILoadingScreen from '@/components/loading/i-loading-screen.vue';
+import IModal from '@/components/modal/i-modal.vue';
+import { ContractEnum } from '@/enums/contract.enum.ts';
+import { EditFormEnum } from '@/enums/edit-form.enum.ts';
+import { RouteNameEnum } from '@/enums/router.enum.ts';
+import { useContactsStore } from '@/stores/contacts.store.ts';
+import { IContact, UpdateContactEmitType } from '@/types';
+import { onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const contactStore = useContactsStore();
+const route = useRoute();
+const router = useRouter();
+const contact = ref<IContact | null>(null);
+const isDeletionModal = ref<boolean>(false);
+const isActionContact = ref<boolean>(false);
+const isEditFormOpened = ref(false);
+const isDeleteContact = ref<boolean>(false);
+const isDeleteStatus = ref<boolean>(false);
+
+const actionContract = async (status: ContractEnum) => {
+  try {
+    await contactStore.actionContract(String(route.params.id), status);
+    contact.value = await contactStore.getContactById(String(route.params.id));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteContact = async () => {
+  isDeleteContact.value = true;
+  try {
+    await contactStore.deleteContact(String(route.params.id));
+    isDeleteStatus.value = true;
+    setTimeout(() => {
+      router.push({ name: RouteNameEnum.Contacts });
+      isDeleteStatus.value = false;
+    }, 700);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isDeleteContact.value = false;
+  }
+};
+
+const closeModal = () => {
+  isDeletionModal.value = false;
+};
+
+const editAction = async (event: UpdateContactEmitType) => {
+  switch (event.status) {
+    case EditFormEnum.CANCEL:
+      isEditFormOpened.value = event.close;
+      break;
+    case EditFormEnum.SAVE:
+      isEditFormOpened.value = event.close;
+      contact.value = await contactStore.getContactById(String(route.params.id));
+  }
+};
+
+onMounted(async () => {
+  try {
+    contact.value = await contactStore.getContactById(String(route.params.id));
+  } catch (error: any) {
+    console.error(error);
+  }
+});
+</script>
+
 <template>
   <i-loading-screen v-if="!contact" />
 
@@ -72,75 +144,3 @@
     </teleport>
   </section>
 </template>
-
-<script setup lang="ts">
-import IContactInfo from '@/components/contact/i-contact-info.vue';
-import IUpdateContact from '@/components/edit-form/i-update-contact.vue';
-import ILoadingScreen from '@/components/loading/i-loading-screen.vue';
-import IModal from '@/components/modal/i-modal.vue';
-import { ContractEnum } from '@/enums/contract.enum.ts';
-import { EditFormEnum } from '@/enums/edit-form.enum.ts';
-import { RouteNameEnum } from '@/enums/router.enum.ts';
-import { useContactsStore } from '@/stores/contacts.store.ts';
-import { IContact, UpdateContactEmitType } from '@/types';
-import { onMounted, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-
-const contactStore = useContactsStore();
-const route = useRoute();
-const router = useRouter();
-const contact = ref<IContact | null>(null);
-const isDeletionModal = ref<boolean>(false);
-const isActionContact = ref<boolean>(false);
-const isEditFormOpened = ref(false);
-const isDeleteContact = ref<boolean>(false);
-const isDeleteStatus = ref<boolean>(false);
-
-const actionContract = async (status: ContractEnum) => {
-  try {
-    await contactStore.actionContract(String(route.params.id), status);
-    contact.value = await contactStore.getContactById(String(route.params.id));
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const deleteContact = async () => {
-  isDeleteContact.value = true;
-  try {
-    await contactStore.deleteContact(String(route.params.id));
-    isDeleteStatus.value = true;
-    setTimeout(() => {
-      router.push({ name: RouteNameEnum.Contacts });
-      isDeleteStatus.value = false;
-    }, 700);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    isDeleteContact.value = false;
-  }
-};
-
-const closeModal = () => {
-  isDeletionModal.value = false;
-};
-
-const editAction = async (event: UpdateContactEmitType) => {
-  switch (event.status) {
-    case EditFormEnum.CANCEL:
-      isEditFormOpened.value = event.close;
-      break;
-    case EditFormEnum.SAVE:
-      isEditFormOpened.value = event.close;
-      contact.value = await contactStore.getContactById(String(route.params.id));
-  }
-};
-
-onMounted(async () => {
-  try {
-    contact.value = await contactStore.getContactById(String(route.params.id));
-  } catch (error: any) {
-    console.error(error);
-  }
-});
-</script>

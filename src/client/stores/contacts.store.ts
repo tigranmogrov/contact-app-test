4f99aafb425702fe58c924fdb0f77ccecad41781
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-24 11:41:13 +0300

    feat(Stores): create getContactsData action

--- src/client/stores/contacts.store.ts
+++ src/client/stores/contacts.store.ts
@@ -1,4 +1,6 @@
-import { IContactData } from '@/types';
+import { API } from '@/api';
+import { config } from '@/config.ts';
+import { ContactsQueryType, IContactData } from '@/types';
 import { defineStore } from 'pinia';
 import { computed, reactive, ref } from 'vue';
 
@@ -19,6 +21,26 @@ export const useContactsStore = defineStore('contacts', () => {
   const fromPageNumber = computed(() => contactData.fromPageNumber);
   const lastPage = computed(() => contactData.toPageNumber === contactData.last);
 
+  const getContactsData = async (queryParams: Partial<ContactsQueryType> = {}): Promise<void> => {
+    try {
+      isLoading.value = true;
+      const { data } = await API.get<IContactData>('/api/contacts', {
+        params: {
+          pageSize: config.contactListPageSize,
+          ...queryParams
+        }
+      });
+      setContactData(data);
+    } catch (error: any) {
+      throw error;
+    } finally {
+      // just to see the loader
+      setTimeout(() => {
+        isLoading.value = false;
+      }, 1000);
+    }
+  };
+
   const setContactData = (data: IContactData): void => {
     contactData.last = data.last;
     contactData.toPageNumber = data.toPageNumber;
@@ -36,6 +58,7 @@ export const useContactsStore = defineStore('contacts', () => {
     contacts,
     fromPageNumber,
     lastPage,
+    getContactsData,
     setContactData
   };
 });

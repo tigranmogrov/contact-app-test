bece204d58f0b6360ef0e63d6eba2dd37bffb04f
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-24 15:35:47 +0300

    feat(Components): create getEmployment action for toolbar

--- src/client/components/toolbar/i-toolbar.vue
+++ src/client/components/toolbar/i-toolbar.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import {
+  useAddQueryParams,
+  useRemoveQueryParams,
+  useTransformQuery
+} from '@/composable/rout-query';
 import { SortCriterion, SortDirection } from '@/enums/sort.enum.ts';
+import { useContactsStore } from '@/stores/contacts.store.ts';
 import { IContactSortOptions } from '@/types';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
-const isEmployment = ref(Boolean(router.currentRoute.value.query.isEmployment) ?? false);
+const contactStore = useContactsStore();
+const addQueryParams = useAddQueryParams();
+const removeQueryParams = useRemoveQueryParams();
+const transformQuery = useTransformQuery();
+const parsedQuery = computed(() => transformQuery(router.currentRoute.value.query));
+const isEmployment = ref(parsedQuery.value['isEmployee'] ?? false);
 const selectedSortOption = ref(1);
 const sortOptions: IContactSortOptions[] = [
   {
@@ -41,6 +52,19 @@ const sortOptions: IContactSortOptions[] = [
     title: `За ім'ям (Я - А)`
   }
 ];
+
+const getEmployment = (value: boolean) => {
+  contactStore.getContactsData({
+    isEmployee: value,
+    fromPageNumber: 1,
+    toPageNumber: 1
+  });
+  if (value) {
+    addQueryParams({ isEmployee: value, fromPageNumber: 1, toPageNumber: 1 });
+  } else {
+    removeQueryParams(['isEmployee', 'fromPageNumber', 'toPageNumber']);
+  }
+};
 </script>
 
 <template>
@@ -59,7 +83,7 @@ const sortOptions: IContactSortOptions[] = [
     </label>
 
     <label class="checkbox">
-      <input v-model="isEmployment" type="checkbox" />
+      <input v-model="isEmployment" type="checkbox" @update:model-value="getEmployment" />
       Показати тільки співробітників
     </label>
   </div>

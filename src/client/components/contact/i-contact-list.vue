ea12ca127c9a258b704ef06accbf1e489c626da1
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-24 13:14:06 +0300

    feat(Component): create contact-list and import contact-item. Import useContactsStore get contacts

---
+++ src/client/components/contact/i-contact-list.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import IContactItem from '@/components/contact/i-contact-item.vue';
+import { useContactsStore } from '@/stores/contacts.store.ts';
+import { storeToRefs } from 'pinia';
+
+const contactStore = useContactsStore();
+const { contacts } = storeToRefs(contactStore);
+</script>
+
+<template>
+  <ul class="contact-list">
+    <i-contact-item v-for="contact in contacts" :key="contact.id" v-bind="contact" />
+  </ul>
+</template>
+
+<style scoped></style>

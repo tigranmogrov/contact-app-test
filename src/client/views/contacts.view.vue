9eab216ff2d357cedbadca26f7166368ed6185dd
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-24 15:01:25 +0300

    refactor(View): remove style and move to styles folder

--- src/client/views/contacts.view.vue
+++ src/client/views/contacts.view.vue
@@ -2,13 +2,10 @@
   <section class="section">
     <div class="container">
       <h1 class="title is-size-4">Список контактів</h1>
-
       <i-toolbar />
 
-      <template v-if="listing">
-        <i-contact-list />
-        <i-pagination />
-      </template>
+      <i-contact-list />
+      <i-pagination />
     </div>
   </section>
 
@@ -21,24 +18,3 @@ import IContactList from '@/components/contact/i-contact-list.vue';
 import IPagination from '@/components/pagination/i-pagination.vue';
 import IToolbar from '@/components/toolbar/i-toolbar.vue';
 </script>
-
-<style>
-.contact-listing {
-  display: grid;
-  gap: 16px;
-}
-.toolbar {
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 16px;
-}
-.add-contact-button {
-  margin-inline-start: auto;
-}
-@media only screen and (max-width: 768px) {
-  .add-contact-button {
-    flex: 1 1 100%;
-  }
-}
-</style>
